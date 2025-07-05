439f7db282fc9cab4770e6e8677bf3a1bc95a765
callumanderson <callum.g.anderson@gmail.com>
2025-07-05 18:40:59 -0400

    duality LP problem implement and solve

--- dual-LP.py
+++ dual-LP.py
@@ -1,9 +1,28 @@
-from scipy.optimize import linprog
-
 """
+300501101 - Callum Anderson
+Assignment 2, Q1, Pt. 2
+
 min:    5x1 + 6x2 
 s.t:    4x1 + 2x2 ≥ 21
         2x1 + 4x2 ≥ 19
         x1, y1 ≥ 0
 """
 
+from pulp import LpProblem, LpVariable, LpStatus, LpMinimize
+
+model = LpProblem(name="Dual-LP-assignment-2", sense=LpMinimize)
+
+x1 = LpVariable(name="x1", lowBound=0) # lowBound = 0 covers "x1, y1 ≥ 0"
+x2 = LpVariable(name="x2", lowBound=0) # lowBound = 0 covers "x1, y1 ≥ 0"
+
+model += (4*x1 + 2*x2 >= 21, "x1")
+model += (2*x1 + 4*x2 >= 19, "x2")
+
+model += 5*x1 + 6*x2
+
+model.solve()
+
+print(f"Status: {LpStatus[model.status]}")
+print(f"x1 = {x1.value()}")
+print(f"x2 = {x2.value()}")
+print(f"Objective value = {model.objective.value()}")
